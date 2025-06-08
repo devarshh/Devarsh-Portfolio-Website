05f594d1f780341593fa076db71b785f04134fda
devarshh <devarshjoshidev@gmail.com>
2025-06-08 16:17:01 -0400

    piano changes 2

--- piano/vite.config.ts
+++ piano/vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/piano/',
+  base: "/piano/", // ✅ this line makes it work under /piano path
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
